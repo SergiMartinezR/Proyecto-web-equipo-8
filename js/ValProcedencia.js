db58839bfcb7061a7cd0c9954bc877f8905cf2ee
SergiMartinezR <sergimartinezr7@gmail.com>
2021-12-03 18:39:46 -0600

    Se agrego la validacion del promedio

--- js/ValProcedencia.js
+++ js/ValProcedencia.js
@@ -27,6 +27,24 @@ function validarSelecciones() {
         alert("Promedio invalido");
         return false;
     }
+    //si cumple la expresion regular, no se muestra el mensaje de corregir
+    if (expresion.test(prom)) {
+        //quita el campo en rojo
+        document.getElementById('promedio').classList.remove('input-field-incorrecto');
+        //pone el campo en verde
+        document.getElementById('promedio').classList.add('input-field-correcto');
+        //se esconde el mensaje
+        document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        //si no cumple la expresion regular, se muestra el mensaje de corregir
+    } else {
+        document.q
+        //quita el campo en rojo
+        document.getElementById(`grupo_${campo}`).classList.add('input-field-incorrecto');
+        //pone el campo en verde
+        document.getElementById(`grupo_${campo}`).classList.remove('input-field-correcto');
+        //se esconde el mensaje
+        document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+    }
     var optESCOM = document.getElementById('escomopcion');
     var verificarOptESCOM = optESCOM.checked;
     if (verificarOptESCOM == null || verificarOptESCOM == '') {
