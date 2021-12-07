4da96192c4f65061f7e401a3deb4de678141bdb7
Jorge Ortega <joredorsi@gmail.com>
2021-12-07 16:42:29 -0600

    Actualizacion4

--- js/validaciones.js
+++ js/validaciones.js
@@ -2,6 +2,28 @@ const formulario = document.getElementById('formulario');//trae el formulario
 const inputs = document.querySelectorAll('#formulario input');//almacena lo que hay en los inputs del formulario
 const selects = document.querySelectorAll('#formulario select');
 
+var nombre = document.getElementById('nombre');
+var boleta = document.getElementById('boleta');
+var apePat = document.getElementById('paterno');
+var apeMate = document.getElementById('materno');
+var nacimiento = document.getElementById('nacimiento');
+var genero = document.getElementById('genero');
+var curp = document.getElementById('curp');
+var dir1 = document.getElementById('direccion');
+var col = document.getElementById('colonia');
+var alcaldia = document.getElementById('alcaldia');
+var CP = document.getElementById('CP');
+var tel = document.getElementById('telefono');
+var correo = document.getElementById('correo');
+var escuela = document.getElementById('escuela');
+var entidad = document.getElementById('entidad');
+var nomescuela = document.getElementById('nomescuela');
+var promedio = document.getElementById('promedio');
+var escomopcion = document.getElementById('escomopcion');
+// Cambia el color de mensaje en este caso del error
+var error = document.getElementById('error');
+error.style.color = "red";
+
 const expresiones = {
     boleta: /^(^PP[\d]{8})|(^PE[\d]{8})|([\d]{10})$/,
     nombre: /^[a-zA-ZÀ-ÿ\s]{3,30}$/,//puede llevar letras mayusculas y minusculas, con acento y espacios, de 3 a 30 caracteres
@@ -15,7 +37,7 @@ const expresiones = {
     CP: /^(\d{5})$/,
     correo: /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/,
     promedio: /^(10+\.\d\d)|([6-9]+\.\d\d)$/,
-    escuelaProcedencia:/^[a-zA-ZÀ-ÿ\s]{3,30}$/
+    escuelaProcedencia: /^[a-zA-ZÀ-ÿ\s]{3,30}$/
 }
 const campos = {
     boleta: false,
@@ -34,7 +56,7 @@ const campos = {
     escuela: false,
     entidad: false,
     promedio: false,
-    nomescuela:false
+    nomescuela: false
 
 }
 const validarIdentidad = (e) => {
@@ -80,17 +102,17 @@ const validarIdentidad = (e) => {
             validarCampos(expresiones.promedio, e.target, e.target.name);//se le pasa la expresion regular, el input a evaluar y el div a cambiar
             break;
         case "alcaldia":
-                validarSelect(e.target, e.target.name);
+            validarSelect(e.target, e.target.name);
             break;
         case "escuela":
-                validarSelect(e.target, e.target.name);
+            validarSelect(e.target, e.target.name);
             break;
         case "entidad":
-                validarSelect(e.target, e.target.name);
+            validarSelect(e.target, e.target.name);
             break;
         case "nomescuela":
-                console.log(e.target.value)
-                validarCampos(expresiones.escuelaProcedencia, e.target, e.target.name);
+            console.log(e.target.value)
+            validarCampos(expresiones.escuelaProcedencia, e.target, e.target.name);
             break;
 
 
@@ -130,77 +152,19 @@ selects.forEach((select) => {
     select.addEventListener('click', validarIdentidad);
     //select.addEventListener('change', validarIdentidad);
 })
-formulario.addEventListener('submit', (e) => {
-    
-    if (campos.nombre && campos.paterno && campos.CP && campos.alcaldia && campos.boleta && campos.colonia 
-        && campos.correo && campos.curp && campos.direccion && campos.materno && campos.telefono
-        && campos.nacimiento && campos.alcaldia && campos.alcaldia && campos.entidad && campos.promedio
-        && (campos.escuela || campos.nomescuela)) {
- 
-        console.log(campos.nombre);   
-        console.log(campos.paterno);   
-        console.log(campos.CP);   
-        console.log(campos.alcaldia);   
-        console.log(campos.boleta);   
-        console.log(campos.colonia);   
-        console.log(campos.correo);   
-        console.log(campos.curp);   
-        console.log(campos.direccion);       
-        console.log(campos.materno);   
-        console.log(campos.telefono);   
-        console.log(campos.nacimiento);    
-        console.log(campos.entidad);    
-        console.log(campos.promedio);    
-        console.log(campos.escuela); 
-        console.log(campos.nomescuela);     
-        console.log("Se envia");
-        
-        formulario.reset();
 
-    } else {
-        console.log(campos.nombre);   
-        console.log(campos.paterno);   
-        console.log(campos.CP);   
-        console.log(campos.alcaldia);   
-        console.log(campos.boleta);   
-        console.log(campos.colonia);   
-        console.log(campos.correo);   
-        console.log(campos.curp);   
-        console.log(campos.direccion);       
-        console.log(campos.materno);   
-        console.log(campos.telefono);   
-        console.log(campos.nacimiento);    
-        console.log(campos.entidad);    
-        console.log(campos.promedio);    
-        console.log(campos.escuela);  
-        console.log(campos.nomescuela); 
-        console.log("No se envia");
-        e.preventDefault();
-        /*document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');*/
-    }
-});
-
-//Funciones
-
-
-//Validacion de identidad
-// Valida que se haya seleccionado una alcaldia 
+// Validacion de los select
 function validarSelect(select, campo) {
-     if (select.value=='0') {
+    if (select.value == '0') {
         document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-        campos[campo] = false;   
-     }else{
+        campos[campo] = false;
+    } else {
         document.querySelector(`#grupo_${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
         campos[campo] = true;
-     }
-    /*var optForm = document.forms["formulario"]["alcaldia"].selectedIndex;
-    if (optForm == null || optForm == 0) {
-        alert("Debe seleccionar una opción en el campo 'Alcadia'");
-        return false;
-    }*/
+    }
 }
 
-//Validaciones de procedencia
+//Validacion del campo otros en la escuela de procedencia
 function showNomEsc(selected) {
     if (selected.value == "oo") {
         document.getElementById("innomescuela").style.display = "inline";
@@ -209,119 +173,126 @@ function showNomEsc(selected) {
     }
 }
 
-// Valida que se hayan seleccionado tanto la escuela de procedencia como la entidad federativa
-/*function validarSelecciones() {
-    var optEsc = document.forms["formulario"]["escuela"].selectedIndex;
-    if (optEsc == null || optEsc == 0) {
-        alert("Debe seleccionar una opción en el campo 'Escuela de procedencia'");
-        return false;
-    }
-    var optEntidad = document.forms["formulario"]["entidad"].selectedIndex;
-    if (optEntidad == null || optEntidad == 0) {
-        alert("Debe seleccionar una opción en el campo 'Entidad federativa'");
-        return false;
-    }
-    /*var prom = document.getElementById('promedio').value;
-    alert(prom);
-    if (isNaN(prom) || prom < 0.00 || prom > 10.00) {
-        alert("Promedio invalido");
-        return false;
-    }*/
-    /*var optESCOM = document.getElementById('escomopcion');
-    var verificarOptESCOM = optESCOM.checked;
-    if (verificarOptESCOM == null || verificarOptESCOM == '') {
-        alert("Debe seleccionar una opción en el campo 'ESCOM fue tu opción:'");
-        return false;
-    }
-    //return true;
-}*/
+formulario.addEventListener('submit', (e) => {
 
+    if (campos.nombre && campos.paterno && campos.CP && campos.alcaldia && campos.boleta && campos.colonia
+        && campos.correo && campos.curp && campos.direccion && campos.materno && campos.telefono
+        && campos.nacimiento && campos.alcaldia && campos.alcaldia && campos.entidad && campos.promedio
+        && (campos.escuela || campos.nomescuela)) {
 
-//Validaciones de campos vacios
-var nombre = document.getElementById('nombre');
-var boleta = document.getElementById('boleta');
-var apePat = document.getElementById('paterno');
-var apeMate = document.getElementById('materno');
-var nacimiento = document.getElementById('nacimiento');
-var genero = document.getElementById('genero');
-var curp = document.getElementById('curp');
-var dir1 = document.getElementById('direccion');
-var col = document.getElementById('colonia');
-var alcaldia = document.getElementById('alcaldia');
-var CP = document.getElementById('CP');
-var tel = document.getElementById('tel');
-var correo = document.getElementById('correo');
-var escuela = document.getElementById('escuela');
-var entidad = document.getElementById('entidad');
-var nomescuela = document.getElementById('nomescuela');
-var promedio = document.getElementById('promedio');
-var escomopcion = document.getElementById('escomopcion');
-// Cambia el color de mensaje en este caso del error
-var error = document.getElementById('error');
-error.style.color = "red";
+        console.log(campos.nombre);
+        console.log(campos.paterno);
+        console.log(campos.CP);
+        console.log(campos.alcaldia);
+        console.log(campos.boleta);
+        console.log(campos.colonia);
+        console.log(campos.correo);
+        console.log(campos.curp);
+        console.log(campos.direccion);
+        console.log(campos.materno);
+        console.log(campos.telefono);
+        console.log(campos.nacimiento);
+        console.log(campos.entidad);
+        console.log(campos.promedio);
+        console.log(campos.escuela);
+        console.log(campos.nomescuela);
+        console.log("Se envia");
+        document.getElementById("formulario__mensaje").classList.add('formulario__mensaje');
+        //pone el campo en verde
+        document.getElementById("formulario__mensaje").classList.remove('formulario__mensaje-activo');
+        formulario.reset();
 
-//Muestra un mensaje con los lementos que no se han respondido
-formulario.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-    var mensajesError = [];
-    if (nombre.value === null || nombre.value === '') {
-        mensajesError.push('Ingresa tu nombre');
-    }
+    } else {
+        console.log(campos.nombre);
+        console.log(campos.paterno);
+        console.log(campos.CP);
+        console.log(campos.alcaldia);
+        console.log(campos.boleta);
+        console.log(campos.colonia);
+        console.log(campos.correo);
+        console.log(campos.curp);
+        console.log(campos.direccion);
+        console.log(campos.materno);
+        console.log(campos.telefono);
+        console.log(campos.nacimiento);
+        console.log(campos.entidad);
+        console.log(campos.promedio);
+        console.log(campos.escuela);
+        console.log(campos.nomescuela);
+        console.log("No se envia");
+        e.preventDefault();
+        var mensajesError = [];
+        if (boleta.value === null || boleta.value === '') {
+            mensajesError.push('Boleta');
+        }
+        if (nombre.value === null || nombre.value === '') {
+            mensajesError.push('Nombre');
+        }
+        if (apePat.value === null || apePat.value === '') {
+            mensajesError.push('Apellido paterno');
+        }
+        if (apeMate.value === null || apeMate.value === '') {
+            mensajesError.push('Apellido materno');
+        }
+        if (nacimiento.value === null || nacimiento.value === '') {
+            mensajesError.push('Fecha de nacimiento');
+        }
+        if (genero.value === null || genero.value === '') {
+            mensajesError.push('Genero');
+        }
+        if (curp.value === null || curp.value === '') {
+            mensajesError.push('Curp');
+        }
+        if (dir1.value === null || dir1.value === '') {
+            mensajesError.push('Calle y numero');
+        }
+        if (col.value === null || col.value === '') {
+            mensajesError.push('Colonia');
+        }
+        if (alcaldia.value === null || alcaldia.value === '') {
+            mensajesError.push('Alcaldia');
+        }
 
-    if (document.getElementById('boleta') === null || document.getElementById('boleta') === '') {
-        mensajesError.push('Ingresa tu boleta');
-    }
-    if (apePat.value === null || apePat.value === '') {
-        mensajesError.push('Ingresa tu Apellido paterno');
-    }
-    if (apeMate.value === null || apeMate.value === '') {
-        mensajesError.push('Ingresa tu apellido materno');
-    }
-    if (nacimiento.value === null || nacimiento.value === '') {
-        mensajesError.push('Selecciona tu fecha de nacimiento');
-    }
-    if (genero.value === null || genero.value === '') {
-        mensajesError.push('Selecciona tu genero');
-    }
-    if (curp.value === null || curp.value === '') {
-        mensajesError.push('Ingresa tu curp');
-    }
-    if (dir1.value === null || dir1.value === '') {
-        mensajesError.push('Ingresa tu calle y numero');
-    }
-    if (col.value === null || col.value === '') {
-        mensajesError.push('Ingresa tu colonia');
-    }
-    if (alcaldia.value === null || alcaldia.value === '') {
-        mensajesError.push('Selecciona tu alcaldia');
-    }
+        if (CP.value === null || CP.value === '') {
+            mensajesError.push('Codigo postal');
+        }
+        if (tel.value === null || tel.value === '') {
+            mensajesError.push('Telefono');
+        }
+        if (correo.value === null || correo.value === '') {
+            mensajesError.push('Correo');
+        }
+        if (escuela.value === null || escuela.value === '') {
+            mensajesError.push('Escuela de procedencia');
+        }
+        if (entidad.value === null || entidad.value === '') {
+            mensajesError.push('Entidad federativa');
+        }
+        if (nomescuela.value === null || nomescuela.value === '') {
+            mensajesError.push('Escuela de procedencia');
+        }
+        if (promedio.value === null || promedio.value === '') {
+            mensajesError.push('Promedio');
+        }
+        if (escomopcion.value === null || escomopcion.value === '') {
+            mensajesError.push('Opcion de ESCOM');
+        }
+        //el formulario se envia
 
-    if (CP.value === null || CP.value === '') {
-        mensajesError.push('Ingresa tu codigo postal');
-    }
-    if (tel.value === null || tel.value === '') {
-        mensajesError.push('Ingresa tu telefono o celular');
-    }
-    if (correo.value === null || correo.value === '') {
-        mensajesError.push('Ingresa tu correo');
-    }
-    if (escuela.value === null || escuela.value === '') {
-        mensajesError.push('Selecciona tu escuela de procedencia');
-    }
-    if (entidad.value === null || entidad.value === '') {
-        mensajesError.push('Selecciona tu entidad');
-    }
-    if (nomescuela.value === null || nomescuela.value === '') {
-        mensajesError.push('Ingresa tu escuela de procedencia');
-    }
-    if (promedio.value === null || promedio.value === '') {
-        mensajesError.push('Ingresa tu promedio');
-    }
-    if (escomopcion.value === null || escomopcion.value === '') {
-        mensajesError.push('Selecciona cual fue tu opcion de escom');
-    }
-    //el formulario se envia
+        error.innerHTML = mensajesError.join(', ');
 
-    error.innerHTML = mensajesError.join(', ');
-
+        document.getElementById("formulario__mensaje").classList.remove('formulario__mensaje');
+        //pone el campo en verde
+        document.getElementById("formulario__mensaje").classList.add('formulario__mensaje-activo');
+    }
 });
+
+//Funciones
+
+
+
+
+
+
+
+
